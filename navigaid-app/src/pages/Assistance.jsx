9d66a2932ea2752521e6e13d90b22216760b599c
yadahampofo <yadahampofo@gmail.com>
2023-11-08 03:57:14 -0500

    add Healthcare Coverage Assistance Organizations

--- navigaid-app/src/pages/Assistance.jsx
+++ navigaid-app/src/pages/Assistance.jsx
@@ -27,26 +27,63 @@ const Container = styled.div`
 const Text = styled.span`
     width: 60%;
     margin-top: 50px;
-    margin-left: 500px;
+    margin-left: 700px;
     margin-right: 2000px;
 `;
 
+
+const data = [
+    { name: "Epiphany Community Healthcare", Location: "Houston TX", Link: "https://echoshouston.org/", LinkText: "Epiphany Community Health Outreach Services"},
+    { name: "Enroll America", Location: "Washington, D.C.", Link: 'https://enrollamerica.org', LinkText: "enrollamerica"},
+    { name: "New York City Human Resources Administration", Location: "New York, NY", Link: "https://www.nyc.gov/site/hra/help/health-assistance.page", LinkText: "New York City Human Resources Administration" },
+    { name: "Healthcare For All", Location: "Boston,MA", Link: "https://hcfama.org/", LinkText: "Healthcare For All" },
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "https://www.unityhealthcare.org" , LinkText: "Unity Healthcare"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male", LinkText: "echoshouston" },
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male", LinkText: "echoshouston" },
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male", LinkText: "echoshouston" },
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+    { name: "Unity Healthcare", Location: "New York, NY", Link: "Male" , LinkText: "echoshouston"},
+]
 
 const Assistance = () => {
     return (
         <div>
             <Container>
             <Text>
-              <h1>Health Outreach Services which help with Medicaid</h1>
+              <h1>Healthcare Coverage Assistance Organization Directory</h1>
             </Text>
             </Container>
-            <h3>Here are some helpful links:</h3>
-            <ul>
-                <li><a href="https://enrollamerica.org/">Enroll America</a></li>
-                <li><a href="https://echoshouston.org/">Epiphany Community Health Outreach Services</a></li>
-                <li><a href="https://www.unityhealthcare.org/">Unity Health Care</a></li>
-                <li><a href="https://www.nyc.gov/site/hra/help/health-assistance.page">New York City Human Resources Administration</a></li>
-            </ul>
+            <table style={{ width: '100%', margin: 20}}>
+                <tr style={{ width: '100%', paddingLeft: 180}}>
+                    <th>Organization</th>
+                    <th>Location</th>
+                    <th style={{ textAlign: 'left', paddingLeft: '200px'}}>Website Link</th>
+                </tr>
+                {data.map((val, key) => {
+                    return (
+                        <tr key={key} style={{ marginBottom: '10px' }}>
+                            <td style={{ textAlign: 'center' , marginBottom: '10px'}}>{val.name}</td>
+                            <td style={{ textAlign: 'center' , marginBottom: '10px'}}>{val.Location}</td>
+                            <a href={val.Link} style={{ textAlign: 'center', paddingLeft: '200px' }}>{val.LinkText}</a>
+                        </tr>
+                    )
+                })}
+            </table>
+
         </div>
     );
 };
